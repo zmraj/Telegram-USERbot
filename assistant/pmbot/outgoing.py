b8801df842ed2b8f551c938f13564320d6069133
Divkix <techdroidroot@gmail.com>
2021-02-22 22:56:24 +0530

    use alt way

--- assistant/pmbot/outgoing.py
+++ assistant/pmbot/outgoing.py
@@ -24,20 +24,17 @@ async def on_out_mssg(event):
     who = event.sender_id
     if x.fwd_from:
         to_user = x.fwd_from.sender_id.user_id
-    else:
-        # this is a weird way of doing it
-        return
-    if who == OWNER_ID:
-        if to_send.startswith("/"):
-            return
-        if event.text is not None and event.media:
-            # if sending media
-            bot_api_file_id = pack_bot_file_id(event.media)
-            await asst.send_file(
-                to_user,
-                file=bot_api_file_id,
-                caption=event.text,
-                reply_to=x.reply_to_msg_id,
-            )
-        else:
-            await asst.send_message(to_user, to_send, reply_to=x.reply_to_msg_id)
+        if who == OWNER_ID:
+            if not to_send.startswith("/"):
+                if event.text is not None and event.media:
+                    # if sending media
+                    bot_api_file_id = pack_bot_file_id(event.media)
+                    await asst.send_file(
+                        to_user,
+                        file=bot_api_file_id,
+                        caption=event.text,
+                        reply_to=x.reply_to_msg_id,
+                    )
+                else:
+                    await asst.send_message(to_user, to_send, reply_to=x.reply_to_msg_id)
+    return
